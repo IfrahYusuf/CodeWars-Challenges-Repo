e82ddea737d679733e3300f9ff1d0040b7520493
Ifrah <ifrah2005@outlook.com>
2022-06-18 20:04:27 +0100

    fourth commit

---
+++ solutions004.js
@@ -0,0 +1,9 @@
+//Complete the solution so that it reverses the string passed into it.
+
+
+function solution(str){
+    //turn the string into an array
+    //reverse the array using the reverse method
+    //use the join method to turn the reversed array into a string
+    return str.split('').reverse().join('')
+  }
